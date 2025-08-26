9387764133b24fc8d0dcbcc7043fe67b45bda4fc
victorbancatelli <victorbancatelli@gmail.com>
2025-08-25 22:10:24 -0300

    feat(script.js, base.css, grid,css): estrutura página e funcionalidade botões

--- Aulas de Web Full Stack/Exercício/script.js
+++ Aulas de Web Full Stack/Exercício/script.js
@@ -1,1 +1,21 @@
 // para rodar o carrossel de imagens
+const carouselImages = document.querySelector('.carousel-images')
+const images = carouselImages.querySelectorAll('img')
+const prevButton = document.querySelector('.prev')
+const nextButton = document.querySelector('.next')
+let currentIndex = 0
+
+function showImage(index){
+    const offset = -index * 100
+    carouselImages.style.transform = `translatex(${offset}%)`
+}
+
+prevButton.addEventListener('click', () =>{
+    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex -1
+    showImage(currentIndex)
+})
+
+nextButton.addEventListener('click', () =>{
+    currentIndex = (currentIndex === images.length -1) ? 0 : currentIndex+1
+    showImage(currentIndex)
+})
